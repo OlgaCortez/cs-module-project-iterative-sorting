f39db688df37151cfc683fcc406475367d005160
Olga Cortez <olgalataviacortez@gmail.com>
2020-07-20 21:13:54 -0500

    Met MVP

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,5 +1,8 @@
 def linear_search(arr, target):
     # Your code here
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
 
 
     return -1   # not found
@@ -7,8 +10,18 @@ def linear_search(arr, target):
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
-
     # Your code here
+    first = 0
+    last = len(arr) - 1
+
+    while first <= last:
+        mid = (first + last) // 2
+        if target == arr[mid]:
+            return mid
+        elif target < arr[mid]:
+            last = mid - 1
+        else:
+            first = mid + 1
 
 
     return -1  # not found
